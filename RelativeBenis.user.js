dc74ad11bcfd5452005dad21c68caa61e61c72fb
Tim Kallage <timkal@me.com>
2015-02-24 01:01:25 +0100

    Ver 0.1

---
+++ RelativeBenis.user.js
@@ -0,0 +1,35 @@
+﻿// ==UserScript==
+// @name		RelativeBenis
+// @author		Chromegear
+// @namespace	chromegear
+// @include		*pr0gramm.com*
+// @version		0.1
+// @copyright	2015+, Chromegear
+// @description	Zeigt auf pr0gramm.com den prozentualen Anteil an Upvotes unter einem Bild an.
+// @icon		http://pr0gramm.com/media/pr0gramm-favicon.png
+// @grant		none
+// ==/UserScript==
+
+$(function() {
+	// Overwrite the "Class" with an extension of itself
+	p.View.Stream.Item = p.View.Stream.Item.extend({
+		template: tmpl_new,
+		// Extend show()
+		show: function(rowIndex, itemData, defaultHeight, jumpToComment) {
+			this.parent(rowIndex, itemData, defaultHeight, jumpToComment);
+			this._BenisInPercent();
+		},
+		// Extend vote(), update our details and bar
+		vote: function(ev, vote) {
+			this.parent(ev, vote);
+			this._BenisInPercent();
+		},
+		_BenisInPercent: function() {
+			var total = this.data.item.up + this.data.item.down;
+			var ratio_up = Math.round(this.data.item.up / total * 100);
+			$('.item-vote .score').css('font-size', '36px');
+			$('.item-vote .score').text(ratio_up + '%');
+		}
+	});
+});
+
