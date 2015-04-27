5adac6394c2b5dba58911e3e5e5cef661b672347
Viken Emesh <vikenemesh@groupc.de>
2015-04-27 20:21:55 +0200

    Bugfix: Ging kaputt, wenn man etwas favorisierte. Rework: Dynamischer Funktionsaufbau

--- RelativeBenis.user.js
+++ RelativeBenis.user.js
@@ -3,34 +3,41 @@
 // @author		Chromegear
 // @namespace	chromegear
 // @include		*pr0gramm.com*
-// @version		1.0
+// @version		1.2
 // @updateURL	https://github.com/Chromegear/RelativeBenis/raw/master/RelativeBenis.user.js
 // @downloadURL	https://github.com/Chromegear/RelativeBenis/raw/master/RelativeBenis.user.js
-// @copyright	2015+, Chromegear
+// @copyright	2015+, Chromegear, vikenzor
 // @description	Zeigt auf pr0gramm.com den prozentualen Anteil an Upvotes unter einem Bild an.
 // @icon		http://pr0gramm.com/media/pr0gramm-favicon.png
 // @grant		none
 // ==/UserScript==
 
 $(function() {
-	p.View.Stream.Item = p.View.Stream.Item.extend({
-		// Extend show()
-		show: function(rowIndex, itemData, defaultHeight, jumpToComment) {
-			this.parent(rowIndex, itemData, defaultHeight, jumpToComment);
+	// Hook in _BenisInPercent for these functions:
+	var extendables = [
+		'show',
+		'vote',
+		'fav'
+	];
+	// This will be the Class definition we use to extend p.View.Stream.Item
+	var extendor = {};
+	
+	// Dynamically build the Class definition
+	extendables.forEach(function(funcname) {
+		extendor[funcname] = function() {
+			this.parent.apply(this, arguments);
 			this._BenisInPercent();
-		},
-		// Extend vote()
-		vote: function(ev, vote) {
-			this.parent(ev, vote);
-			this._BenisInPercent();
-		},
-		// Show score as percentage of upvotes of total vote count
-		_BenisInPercent: function() {
-			var total = this.data.item.up + this.data.item.down;
-			var ratio_up = Math.round(this.data.item.up / total * 100);
-			$('.item-vote .score').css('font-size', '36px');
-			$('.item-vote .score').text(ratio_up + '%');
-		}
+		};
 	});
+	// Add in our own function
+	extendor['_BenisInPercent'] = function() {
+		var item = this.data.item
+		var ratio_up = Math.round(item.up / (item.up + item.down) * 100);
+		$('.item-vote .score')
+			.css( 'font-size', '36px' )
+			.text( ratio_up + '%' );
+	};
+
+	p.View.Stream.Item = p.View.Stream.Item.extend(extendor);
 });
 
